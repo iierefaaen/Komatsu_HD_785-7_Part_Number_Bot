86d7629d4b17db0e3c889b247e159c26297e4be3
iierefaaen <130949431+iierefaaen@users.noreply.github.com>
2024-07-05 10:55:46 +0700

    Update index.js
    update bot
    fetch to api

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+/*
+- komatsu hd 785-7 part number (20000 & 30000 series)
+- using javascript
+- with node-telegram-bot-api
+
+@require: node-telegram-bot-api
+@require: Token from "Bot Father"
+
+----------------------------------------------------
+author: github.com/iierefaaen
+----------------------------------------------------
+=> On this app we just create bot to send
+=> certain "group of part number"
+=> we not use database directly from this bot
+=> but, we consumee api from other repo
+=> (next my repo)
+
+*/
+
+
+// import require package
 const TelegramBot = require('node-telegram-bot-api');
-const mysql = require('mysql')
-const TOKEN = process.env.TELEGRAM_TOKEN || "YOUR_TELEGRAM_BOT_TOKEN"
+// token from bot father
+const TOKEN = "6340529237:AAFNFBGG4xSwYCacdB_InxMI7T1TyWais2U"
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
@@ -8,26 +29,32 @@ const bot = new TelegramBot(TOKEN, {polling: true})
 bot.on('message', (msg) => {
   const keyword = msg.text
   const chatId = msg.chat.id
-  let connection = mysql.createConnection({
-    host: "HOST",
-    user: "USER",
-    password: "YOUR_PASSWORD",
-    database: "YOUR_DB_NAME"
-  })
   
-  connection.connect(function(error) {
-    if (error) throw error
-    // console.log("MYSQL now is connected!...")
-    connection.query(`SELECT * FROM YOUR_DB_NAME WHERE keyword LIKE "%${keyword}%"`, function (error, result) {
-      if (error){
-        throw error
-      }
-      let pesan = []
-      result.forEach(e => pesan.push(e.YOUR_DB_PART_NUM_ROWS))
-      pesan.forEach(el => {
-        bot.sendMessage(chatId, el)
-      })
+  
+  // fetch to api
+  const apiUrl = "http://localhost:3000/peoples/id/" + parseInt(keyword)
+
+fetch(apiUrl)
+  .then(response => {
+    // if response NOT OK
+    if (!response.ok) {
+      console.log('Failed to fetch')
     }
-    )
+    // if response OK
+    return response.json()
   })
-});
+  .then(data => {
+    // Process the retrieved user data
+    let result = data.data // cause data we need place on "data": [{ ... }]
+    console.log(result)
+    for ( let i = 0; i < result.length ; i++ ){
+        //send to user
+        bot.sendMessage(chatId, "message here")
+    }
+  })
+  // Catch Error
+  .catch(error => {
+    console.error("Error fetch : " + error);
+  });
+
+})
